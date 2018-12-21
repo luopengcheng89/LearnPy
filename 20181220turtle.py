c46c0e2a2fca1df91aacad95ef4c6ab72351f101
luopengcheng89 <45779057+luopengcheng89@users.noreply.github.com>
2018-12-21 13:45:41 +0800

    Add files via upload

---
+++ 20181220turtle.py
@@ -0,0 +1,11 @@
+#test
+'''
+  authour:lpc
+  date: 20181220
+'''
+import turtle
+t = turtle.Pen()
+
+for x in range(360):
+    t.forward(x)
+    t.left(100)
---
+++ 20181221foursquare.py
@@ -0,0 +1,70 @@
+#draw four squres
+
+import turtle
+
+turtle.width(5)
+#first square
+turtle.color("red")
+turtle.penup()
+turtle.goto(-20,20)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(150)
+
+turtle.left(90)
+turtle.forward(150)
+
+turtle.left(90)
+turtle.forward(150)
+
+turtle.left(90)
+turtle.forward(150)
+
+#second square
+turtle.color("orange")
+turtle.penup()
+turtle.goto(-20,-20)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+#third square
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(20,-20)
+turtle.pendown()
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+#fourth square
+turtle.color("green")
+turtle.penup()
+turtle.goto(20,20)
+turtle.pendown()
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
+
+turtle.right(90)
+turtle.forward(150)
